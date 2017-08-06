7644ecc066b8559f0d82a5d2189de104f196dba7
Lin David, US-20 <David.Lin@bmwna.com>
2017-08-05 22:14:01 -0500

    initial project files

---
+++ myChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myChat
+//
+//  Created by Lin David, US-20 on 8/5/17.
+//  Copyright © 2017 Lin David, US-20. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
